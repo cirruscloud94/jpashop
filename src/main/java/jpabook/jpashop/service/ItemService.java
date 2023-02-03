b9cd188311b040c2d2c1a18475c4becf160a17b6
Seungju Han <sraq94@gmail.com>
2023-02-03 16:43:01 +0900

    ✈JPA스프링부트 프로젝트 연습

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /** 영속성 컨텍스트가 자동 변경
+     *
+     * @param id
+     * @param name
+     * @param price
+     * @param stockQuantity
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity) {
+
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+    }
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
